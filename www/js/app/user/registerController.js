0673c682fe34409b370390b0b09831de793eb4d2
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2017-05-18 16:03:50 +0000

    Validaciones de registro en email

--- www/js/app/user/registerController.js
+++ www/js/app/user/registerController.js
@@ -1,38 +1,65 @@
 angular.module('registerModule')
 
-.controller('registerController', function($http,$scope, $ionicPopup, $state){
-    
+.controller('registerController', function($http, $scope, $ionicPopup, $state) {
+
     $scope.register = function() {
-  
-    insertarUsuario($http,$scope, $ionicPopup, $state);
-    
-   
+
+        var result = validateEmail($scope.correo);
+        
+        if (result) {
+            insertarUsuario($http, $scope, $ionicPopup, $state);
+        }else{
+            var alertPopup = $ionicPopup.alert({
+                    title: 'Advertencia',
+                    template: 'El correo no es valido'
+                });
+        }
     };
 });
 
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
 
-function insertarUsuario($http,$scope, $ionicPopup, $state){
-     var link = 'https://priscila-backendserve-juanmiguelar09.c9users.io/structure/routers/userRouter.php';
- 
-        $http.post(link, {method:'add', correo : $scope.correo, contrasenna : $scope.contrasenna, nombre : $scope.nombre}).then(function (result){
-            
-            $scope.response = result.data;
-            var respuesta = $scope.response.replace(/['"]+/g, '');
-            if(respuesta == "true"){
+function insertarUsuario($http, $scope, $ionicPopup, $state) {
+    var link = 'https://priscila-backendserve-juanmiguelar09.c9users.io/structure/routers/userRouter.php';
+
+    $http.post(link, {
+        method: 'add',
+        correo: $scope.correo,
+        contrasenna: $scope.contrasenna,
+        nombre: $scope.nombre
+    }).then(function(result) {
+
+        $scope.response = result.data;
+        var respuesta = $scope.response.replace(/['"]+/g, '');
+        if (respuesta == "true") {
+            var alertPopup = $ionicPopup.alert({
+                title: 'Bienvenido a Stanapp',
+                template: 'Se ha registrado con éxito'
+            });
+            alertPopup.then(function(res) {
+                $state.go('app.home');
+            });
+
+        }
+        else {
+
+            if ($scope.correo) {
                 var alertPopup = $ionicPopup.alert({
-                 title: 'Bienvenido a Stanapp',
-                 template: 'Se ha registrado con éxito'
-               });
-               alertPopup.then(function(res) {
-                  $state.go('app.home');
-               });
-               
-            }else{
-                var alertPopup = $ionicPopup.alert({
-                 title: 'Ha ocurrido un error',
-                 template: 'El correo '+ $scope.correo + ' ya existe! Ingrese otro'
-               });
+                    title: 'Ha ocurrido un error',
+                    template: 'El correo ' + $scope.correo + ' ya existe! Ingrese otro'
+                });
             }
-        });
-  }
-  
+            else {
+                var alertPopup = $ionicPopup.alert({
+                    title: 'Advertencia',
+                    template: 'El correo no es valido'
+                });
+            }
+
+
+        }
+    });
+}
