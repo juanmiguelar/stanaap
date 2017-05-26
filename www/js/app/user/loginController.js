db5a78e3dd187f6b097e0c72433974ded60cd542
Priscila <madrigalsolis@hotmail.com>
2017-05-26 05:46:19 +0000

    correcionLogin

--- www/js/app/user/loginController.js
+++ www/js/app/user/loginController.js
@@ -4,7 +4,19 @@ angular.module('loginModule', ['ngStorage'])
     
     $scope.login = function() {
     
-    validarUsuario($http,$scope,$ionicPopup, $state, $localStorage);
+    if($scope.email == null || $scope.password == null){
+        var alertPopup = $ionicPopup.alert({
+                    title: 'Datos incompletos',
+                    template: 'Debe ingresar el email y contraseña'
+                });
+                alertPopup.then(function(res) {
+                    $state.go('app.login');
+                });
+    }else{
+        
+        validarUsuario($http,$scope,$ionicPopup, $state, $localStorage);
+    
+    }
     
     };
 });
@@ -12,35 +24,36 @@ angular.module('loginModule', ['ngStorage'])
 
 function validarUsuario($http,$scope, $ionicPopup, $state, $localStorage){
     
-    $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
      var link = 'https://priscila-backendserve-juanmiguelar09.c9users.io/structure/routers/userRouter.php';
  
         $http.post(link, {method:'validarUsuario', email : $scope.email, password : $scope.password }).then(function (result){
             
             $scope.response = result.data;
             var respuesta = $scope.response;
-           
             //Si la respuesta no es vacia se cumple la condición
-            if(respuesta){
+            if(respuesta[0]["CANTIDAD"] == 1){
                   $localStorage.CORREO_USUARIO = $scope.email;
                   $state.go('app.createReport');
-               
-            }else{
-                var alertPopup = $ionicPopup.alert({
-                 title: 'Datos Inválidos',
-                 template: 'La contraseña o correo son incorrectos.'
-              });
+                
             }
-        }, 
-            function errorCallback(response) {
-             // called asynchronously if an error occurs
-            // or server returns response with an error status.
+            if(respuesta[0]["CANTIDAD"] == 0){
+                var alertPopup = $ionicPopup.alert({
+                     title: 'Datos Inválidos',
+                     template: 'La contraseña o correo son incorrectos.'
+                });
+                
+            }
+        });
+    //     , 
+    //         function errorCallback(response) {
+    //          // called asynchronously if an error occurs
+    //         // or server returns response with an error status.
             
-            var alertPopup = $ionicPopup.alert({
-                 title: '¿Conexión?',
-                 template: 'Al parecer no tienes conexión a internet'
-              });
+    //         var alertPopup = $ionicPopup.alert({
+    //              title: '¿Conexión?',
+    //              template: 'Al parecer no tienes conexión a internet'
+    //           });
             
-      });
+    //   });
   }
   
