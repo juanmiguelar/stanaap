ce028b66ffc405818d883e7ae3a9748c402c5feb
Steven <stevensamc77@gmail.com>
2017-06-10 16:17:27 +0000

    Vamos a probar juntos

--- www/js/app/main/map.js
+++ www/js/app/main/map.js
@@ -2,15 +2,31 @@ var arrayPos = [];
 
 angular.module('mapModule', ['ngStorage'])
 
-.controller('MapCtrl', function($http,$scope){
-     $scope.data = {};
-        
-        initMap();
+.controller('MapCtrl', function($http,$scope, $state, $localStorage){
+    $scope.data = {};
+    mostrarReportes($http, $scope, $state);    
 
 })
+
+// Cargando el array del servidor
+function mostrarReportes($http, $scope, $state) {
+    
+        // Trae la información de los reportes(Adopción y maltrato) con la direccion
+        var link = 'https://priscila-backendserve-juanmiguelar09.c9users.io/structure/routers/reportRouter.php';
+    
+        $http.post(link, {
+            method: 'show'
+        }).then(function successCallback(response) {
+            $scope.arrayCasos = response.data;
+            initMap($scope);
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
+    }
+// 
   
-function initMap() {
-  
+function initMap($scope) {
   var uluru = {lat: 10.087, lng: -84.47};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
@@ -18,46 +34,45 @@ function initMap() {
   });
  
         /*ACA lo del array*/
-        
-    var arrayUbicaciones = [{lat: 10.087, long: -84.47}, {lat: 10.0666667, long: -84.3166667}];
+    // var arrayUbicaciones = [{lat: 10.087, long: -84.47}, {lat: 10.0666667, long: -84.3166667}];
+    var arrayUbicaciones = $scope.arrayCasos;
     var count = Object.keys(arrayUbicaciones).length;
     var infowindow = new google.maps.InfoWindow();
-
     var marker, i, contentString;
     for (i = 0; i < count; i++) {   
-    var latitud = parseFloat(arrayUbicaciones[i].lat);
-    var longitud = parseFloat(arrayUbicaciones[i].long);
-   
-     marker = new google.maps.Marker({
-      position: new google.maps.LatLng(latitud, longitud),
-      map: map,
-      icon: 'img/report.png'
-    }); 
- 
-    contentString = '<div class="list card">' +
-  '<div class="item item-avatar">' +
-    /* '<img src="img/avatar.jpg"> ' + */
-      '<h2>Título del caso </h2>' +
-     '<p>Breve descripcion</p>' +
-    '</div>'+
-  
-    '<div class="item item-image">' +
-      /*'<img src="img/cover.jpg">' +*/
-    '</div>' +
-  
-   '<a class="item item-icon-left assertive" href="#">' +
-      '<i class="icon ion-plus-round"></i>' +
-     'Ver Detalles'+
-    '</a>'+
-    '</div>';
-  
-google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent(contentString);
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
- }
+        var latitud = parseFloat(arrayUbicaciones[i].LATITUD);
+        var longitud = parseFloat(arrayUbicaciones[i].LONGITUD);
+       
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(latitud, longitud),
+          map: map,
+          icon: 'img/report.png'
+        }); 
+     
+        contentString = '<div class="list card">' +
+      '<div class="item item-avatar">' +
+        /* '<img src="img/avatar.jpg"> ' + */
+          '<h2>'+ arrayUbicaciones[i].TITULO + '</h2>' +
+         '<p>'+ arrayUbicaciones[i].DESCRIPCION + '</p>' +
+        '</div>'+
+      
+        '<div class="item item-image">' +
+          /*'<img src="img/cover.jpg">' +*/
+        '</div>' +
+      
+       '<a class="item item-icon-left assertive" href="#">' +
+          '<i class="icon ion-plus-round"></i>' +
+         'Ver Detalles'+
+        '</a>'+
+        '</div>';
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+              infowindow.setContent(contentString);
+              infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
 }
 
 
