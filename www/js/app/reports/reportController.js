cc7afc1089eafed0a8055f905847e9cb285653a7
Steven <stevensamc77@gmail.com>
2017-06-15 18:44:36 +0000

    Foto

--- www/js/app/reports/reportController.js
+++ www/js/app/reports/reportController.js
@@ -1,30 +1,95 @@
-angular.module('reportModule',  ['ngStorage','ngCordova'])
+angular.module('reportModule',  ['ngStorage', 'ngCordova'])
 
 
 .controller('reportController', function($http, $scope, $ionicPopup, $state, $localStorage,
-                                        $scope, $cordovaCamera) {
+                                        $scope, $cordovaCamera, $cordovaFile, $cordovaFileTransfer,
+                                        $cordovaDevice, $ionicPopup, $cordovaActionSheet) {
     ///SCOPES DE CASOS DE MALTRATO O ABANDONO
 //GALERIA
-
-$scope.takePicture = function() {
-        var options = { 
-            quality : 75, 
-            destinationType : Camera.DestinationType.DATA_URL, 
-            sourceType : Camera.PictureSourceType.CAMERA, 
-            allowEdit : true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
+    
+    
+    $scope.image = null;
  
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-            $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-            // An error occured. Show a message to the user
-        });
+  $scope.showAlert = function(title, msg) {
+    var alertPopup = $ionicPopup.alert({
+      title: title,
+      template: msg
+    });
+  };
+    
+    
+    $scope.loadImage = function() {
+  var options = {
+    title: 'Seleccionar Fuente de la Imagen',
+    buttonLabels: ['Usar Galería', 'Usar Cámara'],
+    addCancelButtonWithLabel: 'C',
+    androidEnableCancelButton : true,
+  };
+  $cordovaActionSheet.show(options).then(function(btnIndex) {
+    var type = null;
+    if (btnIndex === 1) {
+      type = Camera.PictureSourceType.PHOTOLIBRARY;
+    } else if (btnIndex === 2) {
+      type = Camera.PictureSourceType.CAMERA;
     }
+    if (type !== null) {
+      $scope.selectPicture(type);
+    }
+  });
+}
+
+
+$scope.selectPicture = function(sourceType) {
+  var options = {
+    quality: 100,
+    destinationType: Camera.DestinationType.FILE_URI,
+    sourceType: sourceType,
+    saveToPhotoAlbum: false
+  };
+ 
+  $cordovaCamera.getPicture(options).then(function(imagePath) {
+    // Grab the file name of the photo in the temporary directory
+    var currentName = imagePath.replace(/^.*[\\\/]/, '');
+ 
+    //Create a new name for the photo
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+ 
+    // If you are trying to load image from the gallery on Android we need special treatment!
+    if ($cordovaDevice.getPlatform() == 'Android' && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
+      window.FilePath.resolveNativePath(imagePath, function(entry) {
+        window.resolveLocalFileSystemURL(entry, success, fail);
+        function fail(e) {
+          console.error('Error: ', e);
+        }
+ 
+        function success(fileEntry) {
+          var namePath = fileEntry.nativeURL.substr(0, fileEntry.nativeURL.lastIndexOf('/') + 1);
+          // Only copy because of access rights
+          $cordovaFile.copyFile(namePath, fileEntry.name, cordova.file.dataDirectory, newFileName).then(function(success){
+            $scope.image = newFileName;
+          }, function(error){
+            $scope.showAlert('Error', error.exception);
+          });
+        };
+      }
+    );
+    } else {
+      var namePath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      // Move the file to permanent storage
+      $cordovaFile.moveFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(function(success){
+        $scope.image = newFileName;
+      }, function(error){
+        $scope.showAlert('Error', error.exception);
+      });
+    }
+  },
+  function(err){
+    // Not always an error, maybe cancel was pressed...
+  })
+}
+
 
     
     $scope.ubicacionAdopcion = function(){
