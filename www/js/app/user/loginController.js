3d3e1d936a3da136a1c7556f0bbbbfef8f3dc5f8
Priscila <madrigalsolis@hotmail.com>
2017-06-20 00:31:40 +0000

    ModificacionMenu

--- www/js/app/user/loginController.js
+++ www/js/app/user/loginController.js
@@ -74,15 +74,4 @@ function validarUsuario($http, $scope, $ionicPopup, $state, $localStorage) {
             });
         }
     });
-    //     , 
-    //         function errorCallback(response) {
-    //          // called asynchronously if an error occurs
-    //         // or server returns response with an error status.
-
-    //         var alertPopup = $ionicPopup.alert({
-    //              title: '¿Conexión?',
-    //              template: 'Al parecer no tienes conexión a internet'
-    //           });
-
-    //   });
 }
