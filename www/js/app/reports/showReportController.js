3d93944a67c63a9392b5d2986382fc8d9c9a5234
Priscila <madrigalsolis@hotmail.com>
2017-06-27 01:38:41 +0000

    buscar

--- www/js/app/reports/showReportController.js
+++ www/js/app/reports/showReportController.js
@@ -2,7 +2,6 @@ angular.module('showReportModule', ['ngStorage'])
 
 .controller('showReportController', function($http, $scope, $ionicPopup, $state,$localStorage) {
    
-        console.log($localStorage.TIPO)
         // Trae la información de los reportes(Adopción y maltrato) con la direccion
         var link = 'https://priscila-backendserve-juanmiguelar09.c9users.io/structure/routers/reportRouter.php';
     
@@ -12,7 +11,6 @@ angular.module('showReportModule', ['ngStorage'])
             tipo: $localStorage.TIPO
         }).then(function successCallback(response) {
             $scope.arrayCasos = response.data
-            console.log(response.data);
           }, function errorCallback(response) {
             //En caso de que ocurra un error
           });
