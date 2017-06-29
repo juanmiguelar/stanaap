c2d05d0e5ccc7a18ca3013ec21944b52dafb427f
Priscila <madrigalsolis@hotmail.com>
2017-06-29 16:57:36 +0000

    side-menu-fixed

--- www/js/app/user/logoutController.js
+++ www/js/app/user/logoutController.js
@@ -2,8 +2,13 @@ angular.module('logoutModule', ['ngStorage'])
 
 .controller('logoutController', function($window,$ionicHistory, $ionicPopup, $state, $localStorage) {
 
-   $state.go('app.login');
-   $window.localStorage.clear();
+   
+    $window.localStorage.clear();
     $ionicHistory.clearCache();
     $ionicHistory.clearHistory();
+     $ionicHistory.nextViewOptions({
+            disableBack: true,
+            historyRoot: true
+        });
+    $state.go('app.login');
 });
